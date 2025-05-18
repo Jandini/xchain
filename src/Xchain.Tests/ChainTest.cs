78b3905d37434e5e63e2bb092a51d0d3db896d39
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-18 23:54:37 +0530

    Update README file and create prerelease package

--- src/Xchain.Tests/ChainTest.cs
+++ src/Xchain.Tests/ChainTest.cs
@@ -1,31 +1,23 @@
-namespace Xchain.Tests
+namespace Xchain.Tests;
+
+[TestCaseOrderer("Xchain.LinkOrderer", "Xchain")]
+public class ChainTest
 {
-    [TestCaseOrderer("Xchain.LinkOrderer", "Xchain")]
-    public class ChainTest
+    [Fact, Link(3)]
+    public void Test1()
     {
-        [Fact, Link(1)]
-        public void Test1()
-        {
-            Thread.Sleep(1000);
-        }
+        Thread.Sleep(1000);
+    }
 
-        [Fact, Link(4)]
-        public void Test2()
-        {
-            Thread.Sleep(1000);
-        }
+    [Fact, Link(2)]
+    public void Test2()
+    {
+        Thread.Sleep(3000);
+    }
 
-        [Fact, Link(3)]
-        public void Test3()
-        {
-            Thread.Sleep(1000);
-        }
-
-        [Fact, Link(2)]
-        public void Test4()
-        {
-            Thread.Sleep(1000);
-        }
-
+    [Fact, Link(1)]
+    public void Test3()
+    {
+        Thread.Sleep(2000);
     }
 }
