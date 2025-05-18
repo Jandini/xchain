fe6651e6e2eb5ea4a8b7a12f6b9be9993d873c20
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-19 04:26:12 +0530

    Add inner exception to the skipped reason

--- src/Xchain/TestChainException.cs
+++ src/Xchain/TestChainException.cs
@@ -1,6 +1,6 @@
 ﻿namespace Xchain;
 
 internal class TestChainException(Exception ex, TestChainErrors errors, string callerName, string callerFilePath, int callerLineNumber) 
-    : Exception($"{callerName} {(errors.Count > 0 ? "skipped" : "failed")} in {Path.GetFileName(callerFilePath)} line {callerLineNumber}.\n{ex.Message}", ex)
+    : Exception($"{callerName} {(errors.Count > 0 ? "skipped" : "failed")} in {Path.GetFileName(callerFilePath)} line {callerLineNumber}.\n{ex.Message}{(ex.InnerException != null ? $"\n{ex.InnerException.Message}" : "")}", ex)
 {
 }
