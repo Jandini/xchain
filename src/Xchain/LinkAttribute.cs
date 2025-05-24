20a8c43b90ac1cfc6db1b917c1316b0df13c5209
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-24 14:25:12 +0530

    Remove Link attribute and place Order property to ChainFact and ChainTheory

--- src/Xchain/LinkAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Xchain;
-
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public class LinkAttribute(int order) : Attribute
-{
-    public int Order { get; } = order;
-    public string GetDisplayName() => $"Order = {Order}";
-}
