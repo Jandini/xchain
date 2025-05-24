5fc9e14d79dcbc3745ec9ab841c7249cb1b2a7dc
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-24 14:26:03 +0530

    Rename LinkOrderer to ChainOrderer and implement support for ChainFact and ChainTheory

---
+++ src/Xchain/ChainOrderer.cs
@@ -0,0 +1,23 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Xchain;
+
+public class ChainOrderer : ITestCaseOrderer
+{
+    public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase 
+        => testCases.Select(tc =>
+        {
+            var method = tc.TestMethod.Method;
+            var factAttr = method.GetCustomAttributes(typeof(ChainFactAttribute).AssemblyQualifiedName).FirstOrDefault();
+            var theoryAttr = method.GetCustomAttributes(typeof(ChainTheoryAttribute).AssemblyQualifiedName).FirstOrDefault();
+
+            int order = factAttr?.GetNamedArgument<int>(nameof(ChainFactAttribute.Order))
+                        ?? theoryAttr?.GetNamedArgument<int>(nameof(ChainTheoryAttribute.Order))
+                        ?? 0;
+
+            return new { TestCase = tc, Order = order };
+        })
+        .OrderBy(x => x.Order)
+        .Select(x => x.TestCase);
+}
