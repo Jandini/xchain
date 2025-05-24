1a801fb0e5f02f63bc4fe1af555e89969ad95edf
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-24 14:27:00 +0530

    Update tests

--- src/Xchain.Tests/ChainTest.cs
+++ src/Xchain.Tests/ChainTest.cs
@@ -1,16 +1,16 @@
 namespace Xchain.Tests;
 
-[TestCaseOrderer("Xchain.LinkOrderer", "Xchain")]
+[TestCaseOrderer("Xchain.ChainOrderer", "Xchain")]
 public class ChainTest(TestChainFixture chain) : IClassFixture<TestChainFixture>
 {
-    [ChainFact, Link(3)]
+    [ChainFact(Order = 3, Name = "Throw Exception")]
     public void Test1() => chain.LinkUnless<Exception>((output) =>
     {
         throw new NotImplementedException();
     });
 
 
-    [ChainFact, Link(2)]
+    [ChainFact(Order = 2, Name = "Sleep 2 seconds")]
     public async Task Test2() => await chain.LinkUnlessAsync<NotImplementedException>(async (output, cancellationToken) =>
     {
         var sleep = output.Get<int>("Sleep");
@@ -18,22 +18,22 @@ public class ChainTest(TestChainFixture chain) : IClassFixture<TestChainFixture>
     });
     
 
-    [ChainFact, Link(1)]
+    [ChainFact(Order = 1, Name = "Sleep 1 second")]
     [ChainTag(Owner = "Kethoneinuo", Category = "Important", Color = "Black")]
     public async Task Test3() => await chain.LinkAsync(async (output, cancellationToken) =>
     {
         const int sleep = 1000;
-        output["Sleep"] = sleep;
+        output["Sleep"] = sleep * 2;
         await Task.Delay(sleep, cancellationToken);
     }, TimeSpan.FromMilliseconds(100));
 
-    [ChainFact, Link(4)]
+    [ChainFact(Order = 4, Name = "Throw Exception")]
     public void Test4() => chain.LinkUnless<Exception>((output) =>
     {
         throw new NotImplementedException();
     });
 
-    [ChainFact, Link(5)]
+    [ChainFact(Order = 5, Name = "Throw Exception")]
     public void Test5() => chain.LinkUnless<Exception>((output) =>
     {
         throw new NotImplementedException();
