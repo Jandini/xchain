7bca421a4d131013f5f2d5643159ab2b5afe04e8
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-24 14:26:52 +0530

    Implement Order property, add Name property to create sequence Display Name with padding

--- src/Xchain/ChainFactAttribute.cs
+++ src/Xchain/ChainFactAttribute.cs
@@ -1,8 +1,15 @@
 ﻿using Xunit;
 
-namespace Xchain
+namespace Xchain;
+
+public sealed class ChainFactAttribute : SkippableFactAttribute
 {
-    public sealed class ChainFactAttribute : SkippableFactAttribute
+    public int Order { get; set; } = 0;
+    public int Pad { get; set; } = 0;
+
+    public string Name
     {
+        get => DisplayName;
+        set => DisplayName = Order == 0 ? value : $"#{Order.ToString().PadLeft(Pad, '0')} | {value}";
     }
 }
