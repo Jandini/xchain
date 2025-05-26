aa49e53e9ebee8bbacc9885171431c547bf857b6
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-26 22:51:16 +0530

    Add failed and warning emojis to the test chain exception and prerelease a new nugget

--- src/Xchain/TestChainException.cs
+++ src/Xchain/TestChainException.cs
@@ -1,6 +1,6 @@
 ﻿namespace Xchain;
 
 internal class TestChainException(Exception ex, TestChainErrors errors, string callerName, string callerFilePath, int callerLineNumber) 
-    : Exception($"{callerName} {(errors.Count > 0 ? "skipped" : "failed")} in {Path.GetFileName(callerFilePath)} line {callerLineNumber}.\n{ex.Message}{(ex.InnerException != null ? $"\n{ex.InnerException.Message}" : "")}", ex)
+    : Exception($"{(errors.Count > 0 ? "⚠️" : "❌")} {callerName} {(errors.Count > 0 ? "skipped" : "failed")} in {Path.GetFileName(callerFilePath)} line {callerLineNumber}.\n{ex.Message}{(ex.InnerException != null ? $"\n{ex.InnerException.Message}" : "")}", ex)
 {
 }
