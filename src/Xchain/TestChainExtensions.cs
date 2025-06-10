a322cef48ab17be18833665f6715fd55e5e2623c
Matt Janda <matt.janda.kingston@gmail.com>
2025-06-10 18:14:02 +0530

    Add output get extension to return any object as string

--- src/Xchain/TestChainExtensions.cs
+++ src/Xchain/TestChainExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Threading;
 using Xunit;
 
 namespace Xchain;
@@ -28,6 +27,20 @@ public static class TestChainExtensions
     /// <exception cref="TestChainOutputMissingException">Thrown if the named output is missing.</exception>
     public static T Get<T>(this TestChainOutput output, string name) =>
         output.ContainsKey(name) ? (T)output[name] : throw new TestChainOutputMissingException(name);
+
+
+    /// <summary>
+    /// Retrieves a value from the test chain output by name and returns it as a string.
+    /// Throws if the key does not exist.
+    /// </summary>
+    /// <param name="output">The test chain output.</param>
+    /// <param name="name">The key name of the output item.</param>
+    /// <returns>The string representation of the value.</returns>
+    /// <exception cref="TestChainOutputMissingException">Thrown if the named output is missing.</exception>
+    public static string Get(this TestChainOutput output, string name) =>
+        output.ContainsKey(name)
+            ? output[name]?.ToString() ?? string.Empty
+            : throw new TestChainOutputMissingException(name);
 
     /// <summary>
     /// Executes a test step and captures any exception into the chain's error stack.
