903b5bb5cd69aa5097833403b922aeafd632e99a
Matt Janda <matt.janda.kingston@gmail.com>
2025-06-10 18:14:08 +0530

    Code cleanup

--- src/Xchain/ChainOrderer.cs
+++ src/Xchain/ChainOrderer.cs
@@ -5,19 +5,18 @@ namespace Xchain;
 
 public class ChainOrderer : ITestCaseOrderer
 {
-    public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase 
-        => testCases.Select(tc =>
-        {
-            var method = tc.TestMethod.Method;
-            var factAttr = method.GetCustomAttributes(typeof(ChainFactAttribute).AssemblyQualifiedName).FirstOrDefault();
-            var theoryAttr = method.GetCustomAttributes(typeof(ChainTheoryAttribute).AssemblyQualifiedName).FirstOrDefault();
+    public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase => testCases.Select(tc =>
+    {
+        var method = tc.TestMethod.Method;
+        var factAttr = method.GetCustomAttributes(typeof(ChainFactAttribute).AssemblyQualifiedName).FirstOrDefault();
+        var theoryAttr = method.GetCustomAttributes(typeof(ChainTheoryAttribute).AssemblyQualifiedName).FirstOrDefault();
 
-            int order = factAttr?.GetNamedArgument<int>(nameof(ChainFactAttribute.Link))
-                        ?? theoryAttr?.GetNamedArgument<int>(nameof(ChainTheoryAttribute.Link))
-                        ?? 0;
+        int order = factAttr?.GetNamedArgument<int>(nameof(ChainFactAttribute.Link))
+                    ?? theoryAttr?.GetNamedArgument<int>(nameof(ChainTheoryAttribute.Link))
+                    ?? 0;
 
-            return new { TestCase = tc, Order = order };
-        })
-        .OrderBy(x => x.Order)
-        .Select(x => x.TestCase);
+        return new { TestCase = tc, Order = order };
+    })
+    .OrderBy(x => x.Order)
+    .Select(x => x.TestCase);
 }
