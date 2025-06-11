1c9766e622f20aecaa967dd07970acba6b690702
Matt Janda <matt.janda.kingston@gmail.com>
2025-06-11 13:39:16 +0530

    Add chain linker based on collection orderer

---
+++ src/Xchain/ChainLinker.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Xchain;
+
+/// <summary>
+/// Orders test collections by the Link value defined in [ChainLink].
+/// Collections without a Link default to 0.
+/// </summary>
+public class ChainLinker : ITestCollectionOrderer
+{
+    public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+    {
+        return testCollections
+            .Select(tc =>
+            {
+                var attr = tc.CollectionDefinition?.GetCustomAttributes(typeof(ChainLinkAttribute).AssemblyQualifiedName!)
+                    .FirstOrDefault();
+
+                int link = attr?.GetNamedArgument<int>(nameof(ChainLinkAttribute.Link)) ?? 0;
+
+                return new { Collection = tc, Link = link };
+            })
+            .OrderBy(x => x.Link)
+            .Select(x => x.Collection);
+    }
+}
