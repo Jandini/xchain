951e94affc38d325d58275054f830d54e9dd860d
Matt Janda <matt.janda.kingston@gmail.com>
2025-06-13 12:52:13 +0530

    Unseal fact and theory attributes and add flow property

--- src/Xchain/ChainTheoryAttribute.cs
+++ src/Xchain/ChainTheoryAttribute.cs
@@ -2,14 +2,14 @@
 
 namespace Xchain;
 
-public sealed class ChainTheoryAttribute : SkippableTheoryAttribute
+public class ChainTheoryAttribute : SkippableTheoryAttribute
 {
     public int Link { get; set; } = 0;
     public int Pad { get; set; } = 0;
-
+    public string Flow { get; set; } = string.Empty;
     public string Name
     {
         get => DisplayName;
-        set => DisplayName = Link == 0 ? value : $"#{Link.ToString().PadLeft(Pad, '0')} | {value}";
-    }    
+        set => DisplayName = Link == 0 ? value : $"#{Link.ToString().PadLeft(Pad, '0')} | {(string.IsNullOrEmpty(Flow) ? "" : Flow + " | ")}{value}";
+    }
 }
