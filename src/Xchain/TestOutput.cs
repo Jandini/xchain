b5516bc6b0c78ceb793576c57ebb43d572b317ae
Matt Janda <matt.janda.kingston@gmail.com>
2025-06-18 16:58:12 +0530

    Add try get from the output

--- src/Xchain/TestOutput.cs
+++ src/Xchain/TestOutput.cs
@@ -21,4 +21,15 @@ public class TestOutput<T, TOutput>(TestChainOutput output, string suffix = null
     /// </summary>
     /// <param name="value">The value to store, expected to be of type <typeparamref name="TOutput"/>.</param>
     public void Put(object value) => output[$"{typeof(T).Name}{(suffix != null ? $"_{suffix}" : string.Empty)}"] = value;
+
+
+    /// <summary>
+    /// Attempts to retrieve a value of type <typeparamref name="TOutput"/> from the output dictionary
+    /// using a key composed of <typeparamref name="T"/>'s type name and an optional suffix.
+    /// </summary>
+    /// <param name="value">When this method returns, contains the value associated with the key,
+    /// if found and of the correct type; otherwise, the default value for <typeparamref name="TOutput"/>.</param>
+    /// <returns><c>true</c> if the value was found and is of type <typeparamref name="TOutput"/>; otherwise, <c>false</c>.</returns>
+    public bool TryGet(out TOutput value) => output.TryGetValue($"{typeof(T).Name}{(suffix != null ? $"_{suffix}" : string.Empty)}", out var obj) && obj is TOutput v && (value = v) != null || (value = default) == null;
+
 }
