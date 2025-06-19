92d8c721d2cfd5203dcdcfe0a1e19f03fe43001e
Matt Janda <matt.janda.kingston@gmail.com>
2025-06-19 11:41:35 +0530

    Add collection link extensions with variable return and create prerelease nuget

--- src/Xchain/CollectionChainContextFixtureExtensions.cs
+++ src/Xchain/CollectionChainContextFixtureExtensions.cs
@@ -26,14 +26,38 @@ namespace Xchain
         }
 
 
-        public static async Task LinkWithCollectionAsync<TCollection, TOutput>(this CollectionChainContextFixture fixture, TestOutput<TCollection, TOutput> output, Func<TestChainOutput, CancellationToken, Task> linkAction, TimeSpan timeOut = default,
+        public static TOutput LinkWithCollection<TCollection, TOutput>(this CollectionChainContextFixture fixture, TestOutput<TCollection, TOutput> output, Func<TestChainOutput, TOutput> linkAction, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = -1)
+            => fixture.LinkWithCollection<TCollection, TOutput>(output.Key, linkAction, callerName, callerFilePath, callerLineNumber);
+
+        public static TResult LinkWithCollection<TCollection, TResult>(this CollectionChainContextFixture fixture, string outputKey, Func<TestChainOutput, TResult> linkAction,
             [CallerMemberName] string callerName = "",
             [CallerFilePath] string callerFilePath = "",
             [CallerLineNumber] int callerLineNumber = -1)
         {
             try
             {
-                Skip.IfNot(output.ContainsKey(), $"The expected output {output.Key} was not provided by {typeof(TCollection)}.");
+                Skip.IfNot(fixture.Output.ContainsKey(outputKey), $"The expected output {outputKey} was not provided by {typeof(TCollection)}.");
+                return linkAction(fixture.Output);
+            }
+            catch (Exception ex)
+            {
+                fixture.Errors.Push(new TestChainException(ex, fixture.Errors, callerName, callerFilePath, callerLineNumber));
+                throw;
+            }
+        }
+
+
+        public static async Task LinkWithCollectionAsync<TCollection, TOutput>(this CollectionChainContextFixture fixture, TestOutput<TCollection, TOutput> output, Func<TestChainOutput, CancellationToken, Task> linkAction, TimeSpan timeOut = default, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = -1)
+            => await fixture.LinkWithCollectionAsync<TCollection, TOutput>(output.Key, linkAction, timeOut, callerName, callerFilePath, callerLineNumber);
+        
+        public static async Task LinkWithCollectionAsync<TCollection, TOutput>(this CollectionChainContextFixture fixture, string outputKey, Func<TestChainOutput, CancellationToken, Task> linkAction, TimeSpan timeOut = default,
+            [CallerMemberName] string callerName = "",
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = -1)
+        {
+            try
+            {
+                Skip.IfNot(fixture.Output.ContainsKey(outputKey), $"The expected output {outputKey} was not provided by {typeof(TCollection)}.");
                 using CancellationTokenSource cts = timeOut != default ? new(timeOut) : new();
                 await linkAction(fixture.Output, cts.Token);
             }
@@ -52,68 +76,32 @@ namespace Xchain
 
 
 
+        public static async Task<TOutput> LinkWithCollectionAsync<TCollection, TOutput>(this CollectionChainContextFixture fixture, TestOutput<TCollection, TOutput> output, Func<TestChainOutput, CancellationToken, Task<TOutput>> linkAction, TimeSpan timeOut = default, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = -1)
+            => await fixture.LinkWithCollectionAsync<TCollection, TOutput>(output.Key, linkAction, timeOut, callerName, callerFilePath, callerLineNumber);
 
-
-        //    public static async Task<TResult> LinkCollectionAsync<TResult>(this CollectionChainContextFixture fixture, Func<TestChainOutput, CancellationToken, Task<TResult>> linkAction,
-        //        TimeSpan timeOut = default,
-        //        [CallerMemberName] string callerName = "",
-        //        [CallerFilePath] string callerFilePath = "",
-        //        [CallerLineNumber] int callerLineNumber = -1)
-        //    {
-        //        try
-        //        {
-        //            using CancellationTokenSource cts = timeOut != default ? new(timeOut) : new();
-        //            return await linkAction(fixture.Output, cts.Token);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            fixture.Errors.Push(new TestChainException(ex, fixture.Errors, callerName, callerFilePath, callerLineNumber));
-        //            throw;
-        //        }
-        //    }
-
-
-        //    public static async Task LinkCollectionUnlessAsync<TException>(this CollectionChainContextFixture fixture, Func<TestChainOutput, CancellationToken, Task> linkAction,
-        //        TimeSpan timeOut = default,
-        //        [CallerMemberName] string callerName = "",
-        //        [CallerFilePath] string callerFilePath = "",
-        //        [CallerLineNumber] int callerLineNumber = -1) where TException : Exception
-        //    {
-        //        try
-        //        {
-        //            var exception = fixture.Errors.FirstOrDefault(ex => ex.InnerException is TException);
-        //            Skip.If(exception is not null, exception?.Message);
-        //            using CancellationTokenSource cts = timeOut != default ? new(timeOut) : new();
-        //            await linkAction(fixture.Output, cts.Token);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            fixture.Errors.Push(new TestChainException(ex, fixture.Errors, callerName, callerFilePath, callerLineNumber));
-        //            throw;
-        //        }
-        //    }
-
-
-        //    public static async Task<TResult> LinkCollectionUnlessAsync<TException, TResult>(this CollectionChainContextFixture fixture, Func<TestChainOutput, CancellationToken, Task<TResult>> linkAction,
-        //        TimeSpan timeOut = default,
-        //        [CallerMemberName] string callerName = "",
-        //        [CallerFilePath] string callerFilePath = "",
-        //        [CallerLineNumber] int callerLineNumber = -1) where TException : Exception
-        //    {
-        //        try
-        //        {
-        //            var exception = fixture.Errors.FirstOrDefault(ex => ex.InnerException is TException);
-        //            Skip.If(exception is not null, exception?.Message);
-        //            using CancellationTokenSource cts = timeOut != default ? new(timeOut) : new();
-        //            return await linkAction(fixture.Output, cts.Token);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            fixture.Errors.Push(new TestChainException(ex, fixture.Errors, callerName, callerFilePath, callerLineNumber));
-        //            throw;
-        //        }
-        //    }
-        //}
+        public static async Task<TOutput> LinkWithCollectionAsync<TCollection, TOutput>(this CollectionChainContextFixture fixture, string outputKey, Func<TestChainOutput, CancellationToken, Task<TOutput>> linkAction, TimeSpan timeOut = default,
+            [CallerMemberName] string callerName = "",
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = -1)
+        {
+            try
+            {
+                Skip.IfNot(fixture.Output.ContainsKey(outputKey), $"The expected output {outputKey} was not provided by {typeof(TCollection)}.");
+                using CancellationTokenSource cts = timeOut != default ? new(timeOut) : new();
+                return await linkAction(fixture.Output, cts.Token);
+            }
+            catch (OperationCanceledException ex) when (timeOut != default)
+            {
+                var tex = new TimeoutException($"The {callerName} timed out after {timeOut}.", ex);
+                fixture.Errors.Push(new TestChainException(tex, fixture.Errors, callerName, callerFilePath, callerLineNumber));
+                throw tex;
+            }
+            catch (Exception ex)
+            {
+                fixture.Errors.Push(new TestChainException(ex, fixture.Errors, callerName, callerFilePath, callerLineNumber));
+                throw;
+            }
+        }
     }
 }
 
